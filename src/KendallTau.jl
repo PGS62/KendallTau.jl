bd37451442b3bf1ea6aafef9a0ddfd8276c1c22c
Philip Swannell <philip.swannell@gmail.com>
2021-01-23 19:25:10 +0000

    Try to get build working on Julia 1.0

--- src/KendallTau.jl
+++ src/KendallTau.jl
@@ -9,9 +9,11 @@ const RealVector{T <: Real} = AbstractArray{T,1}
 const RealMatrix{T <: Real} = AbstractArray{T,2}
 
 include("rankcorr.jl")
-include("threads_v1.jl")
-include("threads_v2.jl")
-include("threads_v3.jl")
+if VERSION >= v"1.5" #@spawn not available on 1.0
+    include("threads_v1.jl")
+    include("threads_v2.jl")
+    include("threads_v3.jl")
+end
 include("speedtests.jl")
 
 export corkendall
