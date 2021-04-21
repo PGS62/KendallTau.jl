e743cd270dc750367ae68c82d2ff5752f309cb26
Philip Swannell <philip.swannell@gmail.com>
2021-04-21 19:25:53 +0100

    skipmissingpairs functor

--- src/skipmissingpairs.jl
+++ src/skipmissingpairs.jl
@@ -1,4 +1,106 @@
 """
+    skipmissingpairs(fn::Function)::Function
+Given a function `fn` with a method `fn(x::RealVector,y::RealVector)::Float64` this function
+returns a function `fn_out` with five methods. MORE CHAT HERE.
+    
+For example, if `X` is of type `Union{RealMatrix,RealOrMissingMatrix}` then `fn_out(X)`
+returns a matrix `C` where `C[i,j] == fn(coli,colj)` and
+`(coli,colj) = skipmissingpairs(X[:,i],X[:,j])`
+
+
+# Example
+```
+julia> x = [missing;1;2;5;4;missing];
+
+julia> y = [1;3;missing;2;3;6];
+
+julia> hcat(x,y)
+6×2 Matrix{Union{Missing, Int64}}:
+  missing  1
+ 1         3
+ 2          missing
+ 5         2
+ 4         3
+  missing  6
+
+julia> KendallTau.skipmissingpairs(StatsBase.corkendall)(x,y)
+-0.8164965809277261
+
+julia> StatsBase.corkendall([1;5;4],[3;2;3])
+-0.8164965809277261
+```
+"""
+function skipmissingpairs(fn::Function)::Function
+
+	function smp(f::Function)
+		function f2(x::RealVector,y::RealVector)
+			f(x,y)
+		end
+		function f2(x::RealOrMissingVector,y::RealOrMissingVector)
+			x,y = skipmissingpairs(x,y)
+			f(x,y)
+		end
+		f2
+	end
+
+	fn_smp = smp(fn)
+
+	function fn_out(x::Union{RealVector,RealOrMissingVector},
+		y::Union{RealVector,RealOrMissingVector})
+		length(x) == length(y) || throw(DimensionMismatch("Vectors must have same length"))
+		fn_smp(x,y)
+	end
+
+	function fn_out(X::Union{RealMatrix,RealOrMissingMatrix})
+		n = size(X, 2)
+		C = Matrix{Float64}(I, n, n)
+		for j = 2:n
+			for i = 1:j - 1
+				C[i,j] = C[j,i] = fn_smp(X[:,j], X[:,i])
+			end
+		end
+		return C
+	end
+
+	function fn_out(x::Union{RealVector,RealOrMissingVector},
+						Y::Union{RealMatrix,RealOrMissingMatrix})
+		size(Y, 1) == length(x) ||
+			throw(DimensionMismatch("x and Y have inconsistent dimensions"))    
+		n = size(Y, 2)
+		return(reshape([fn_smp(x, Y[:,i]) for i in 1:n], 1, n))
+	end
+
+	#TODO Correct this...
+	#= It is idiosyncratic that this method returns a vector, not a matrix, i.e. not consistent
+	with Statistics.cor or corspearman. But fixing that is a breaking change. =#
+	function fn_out(X::Union{RealMatrix,RealOrMissingMatrix},
+		y::Union{RealVector,RealOrMissingVector})
+		
+		size(X, 1) == length(y) ||
+			throw(DimensionMismatch("X and y have inconsistent dimensions"))
+		n = size(X, 2)
+		return([fn_smp(y, X[:,i]) for i in 1:n])
+	end
+
+	function fn_out(X::Union{RealMatrix,RealOrMissingMatrix},
+                    Y::Union{RealMatrix,RealOrMissingMatrix})
+		nr = size(X, 2)
+		nc = size(Y, 2)
+		C = Matrix{Float64}(undef, nr, nc)
+		for j = 1:nr
+			for i = 1:nc
+				C[j,i] = fn_smp(X[:,j], Y[:,i])
+			end
+		end
+		return C
+	end
+
+	return(fn_out)
+
+end
+
+
+"""
     skipmissingpairs(x::RealOrMissingVector, y::RealOrMissingVector)
 Returns	a pair `(a,b)`, filtered copies of `x` and `y`, in which elements `x[i]` and `y[i]`
 are "skipped" (filtered out) if either `ismissing(x[i])` or `ismissing(y[i])`.
