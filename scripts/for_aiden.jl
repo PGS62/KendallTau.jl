a76f29161964cd79ae325053ccdede7cb5a8b81c
Philip Swannell <18028484+PGS62@users.noreply.github.com>
2024-01-18 19:19:28 +0000

    changes to corkendall_fromfile and subroutines

--- scripts/for_aiden.jl
+++ scripts/for_aiden.jl
@@ -15,30 +15,41 @@ julia> @time test_corkendall_fromfile()
 (1.683168734664675e-5, 1.734723475976807e-18)
 ```
  """
-function test_corkendall_fromfile()
+function test_corkendall_fromfile(year=2023)
 
-    base_folder = "C:/Users/phili/OneDrive/ISDA SIMM/Solum Validation C-VIII 2023/EQ_delta"
+    if year == 2023
+        #ISDA's file for 2023 is kendall tau, not converted to Pearson
+        converttopearson = false
+        base_folder = "C:/Users/phili/OneDrive/ISDA SIMM/Solum Validation C-VIII 2023/EQ_delta"
+        #ISDA's calculation of KendallTau
+        file_isda = joinpath(base_folder, "9_correlations/eq_delta-kendall_recent_1-10d.csv")
+        #ISDA's returns data
+        file1 = joinpath(base_folder, "7_returns_relevant_period/returns-10d_recent_1.csv")
+        outputfile = "c:/temp/correlations_2023.csv"
+    elseif year == 2024
+        #ISDA's file for 2024 is kendall tau, converted to Pearson ρ = sin(τπ/2)
+        converttopearson = true
+        base_folder = raw"C:\Users\phili\OneDrive\ISDA SIMM\Solum Validation C-IX 2024\EQ_Delta"
+        #ISDA's calculation of KendallTau
+        file_isda = joinpath(base_folder, raw"9_correlations\eq_delta-individual_recent_0-10d.csv")
+        #ISDA's returns data
+        file1 = joinpath(base_folder, raw"7_returns_relevant_period\returns-10d_recent_0.csv")
+        outputfile = "c:/temp/correlations_2024.csv"
+    else
+        throw("year must be 2023 or 2024 but got $year")
+    end
 
-    #ISDA's calculation of KendallTau
-    file_isda = joinpath(base_folder, "9_correlations/eq_delta-kendall_recent_1-10d.csv")
-
-    #ISDA's raw data
-    file1 = joinpath(base_folder, "7_returns_relevant_period/returns-10d_recent_1.csv")
     file2 = ""
-    outputfile = "c:/temp/test.csv"
+
     inputshaveheaderrow = true
     inputshaveheadercol = true
     writeheaders = true
-    converttopearson = false
     missingstring = ""
     whattoreturn = "filename"
 
     corkendall_fromfile(file1, file2, outputfile, inputshaveheaderrow, inputshaveheadercol,
         writeheaders, converttopearson, missingstring, whattoreturn)
 
-    absdiffs = (abs.(KendallTau.readfromcsv(outputfile, true, true)[1] .-
-                     KendallTau.readfromcsv(file_isda, true, true)[1]))
-
-    maximum(absdiffs), median(absdiffs)
+    KendallTau.comparecorrelationfiles(outputfile, file_isda)
 
 end
