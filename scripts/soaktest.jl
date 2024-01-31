1371c47987021bc5573b8976021637480e9eaf9c
Philip Swannell <18028484+PGS62@users.noreply.github.com>
2024-01-31 16:52:57 +0000

    WIP soaktest

--- scripts/soaktest.jl
+++ scripts/soaktest.jl
@@ -1,4 +1,4 @@
-using KendallTau
+#=using KendallTau
 using Random, StatsBase
 using Test
 
@@ -17,3 +17,21 @@ bigtau = KendallTau.corkendall(bigx)
 @test smalltau == KendallTau.corkendall(smallx)
 @test bigtau == repeat(smalltau, numcopies, numcopies)
 
+=#
+
+using KendallTau
+using Random, StatsBase
+using Test
+
+numcopies = 1
+
+smallx = randn(MersenneTwister(123), 1000, 3)
+indicators = rand(MersenneTwister(456), 1000, 3) .< 0.05
+smallx = ifelse.(indicators,missing,smallx)
+
+#no multithreading used in call below
+smalltau = [KendallTau.corkendall(smallx[:,i],smallx[:,j],skipmissing=:pairwise) for i in 1:3, j in 1:3 ]
+
+bigx = repeat(smallx, 1, numcopies)
+bigtau = KendallTau.corkendall(bigx,skipmissing=:pairwise)
+@test bigtau == repeat(smalltau, numcopies, numcopies)
