67d95a7f06fdcd239aeb43617c497ea48d865341
Philip Swannell <18028484+PGS62@users.noreply.github.com>
2024-03-17 10:00:15 +0000

    merged corspearman.jl and corkendall.jl to rankcorr.jl

--- src/KendallTau.jl
+++ src/KendallTau.jl
@@ -3,9 +3,8 @@ using StatsBase: cor, cov
 using Missings: disallowmissing
 import StatsBase: _tiedrank!, tiedrank #TODO Remove this line on porting to StatsBase
 
-include("corkendall.jl")
+include("rankcorr.jl")
 include("corkendall_fromfile.jl")
-include("corspearman.jl")
 include("pairwise.jl")
 
 export corkendall, corkendall_fromfile, corspearman, pairwise, pairwise!
