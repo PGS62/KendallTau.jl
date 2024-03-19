d50533687a1a0d43d4f1dda05329bb11d2e832d4
Philip Swannell <18028484+PGS62@users.noreply.github.com>
2024-03-19 17:12:22 +0000

    Update _notes.jl

--- src/_notes.jl
+++ src/_notes.jl
@@ -52,4 +52,55 @@ Consider kernel functions taking x and y as arguments so they can do the x===y t
 Check that the tests here are correctly a superset of the tests currently in StatsBase. [DONE]
 I think the call stack described above is one layer too deep, thanks to new fn _pairwise!.
     Would be better to reduce that. [DONE]
+
+Speedup with fast kernel function (LinearAlgebra.dot)
+
+
+julia> using StatsBase,KendallTau
+
+julia> x = rand(1000,1000);
+
+julia> xm=  @. ifelse(x<.01,missing,x);
+
+julia> StatsBase.pairwise(dot,eachcol(xm),skipmissing=:pairwise);#compile
+
+julia> KendallTau.pairwise(dot,eachcol(xm),skipmissing=:pairwise);#compile
+
+julia> @time res_sb=StatsBase.pairwise(dot,eachcol(xm),skipmissing=:pairwise);
+  4.238540 seconds (5.00 M allocations: 19.097 GiB, 14.19% gc time)
+
+julia> @time res_kt=KendallTau.pairwise(dot,eachcol(xm),skipmissing=:pairwise);
+  0.098913 seconds (3.00 M allocations: 114.812 MiB, 0.00% compilation time)
+
+julia> res_sb ≈ res_kt
+true
+
+julia> maximum(abs.(res_sb.-res_kt))
+1.0800249583553523e-12
+
+julia> 4.238540/0.098913
+42.85119246206263
+
+##################################################################
+
+julia> x=rand(1000,1000);
+
+julia> xm=  @. ifelse(x<.01,missing,x);
+
+julia> @btime res_sb=StatsBase.pairwise(dot,eachcol(x));
+  115.311 ms (7 allocations: 7.63 MiB)
+
+julia> @btime res_kt=KendallTau.pairwise(dot,eachcol(x));
+  22.138 ms (3000221 allocations: 114.49 MiB)
+
+julia> res_sb==res_kt
+false
+
+julia> res_sb≈res_kt
+true
+
+julia> maximum(abs.(res_sb.-res_kt))
+1.0800249583553523e-12
+
+
 =#
