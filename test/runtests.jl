215e2752a47485de848b041a5c0702046645f366
Philip Swannell <18028484+PGS62@users.noreply.github.com>
2024-04-07 11:36:22 +0100

    Omit testing corkendall_fromfile on Julia 1.3 and below

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,9 @@ using Test
 
 include("naive_implementations.jl")
 include("compare_implementations.jl")
-include("corkendall_fromfile.jl")
+if VERSION >= v"1.3"# otherwise "Unsatisfiable requirements detected for package CSV [336ed68f]:"
+    include("corkendall_fromfile.jl")
+end
 include("versus_naive.jl")
 include("rankcorr.jl")
 include("pairwise.jl")
